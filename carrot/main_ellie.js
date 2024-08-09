126c02cae0f4cc0fd52c33c60225bde470431f29
김태림 <usinia@nexon.co.kr>
2024-08-09 13:39:37 +0900

    ch08 - game start with ellie

--- carrot/main_ellie.js
+++ carrot/main_ellie.js
@@ -1,19 +1,85 @@
 "use strict";
 
+const IMAGE_SIZE = 80;
+const COUNT = 5;
+const GAME_DURATION = 5;
+
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
-const imageSize = 80;
+const gameBtn = document.querySelector(".game__button");
+const gameTimer = document.querySelector(".game__timer");
+const gameScore = document.querySelector(".game__score");
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener("click", () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+  startGameTimer();
+}
+
+function stopGame() {}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector(".fa-play");
+  icon.classList.add("fa-stop");
+  icon.classList.remove("fa-play");
+}
+
+function showStartButton() {
+  const icon = gameBtn.querySelector(".fa-stop");
+  icon.classList.add("fa-play");
+  icon.classList.remove("fa-stop");
+}
+
+function showTimerAndScore() {
+  gameTimer.style.visibility = "visible";
+  gameScore.style.visibility = "visible";
+}
+
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes}:${seconds}`;
+}
 
 function initGame() {
-  addItem("carrot", 5, "img/carrot.png");
-  addItem("bug", 5, "img/bug.png");
+  field.innerHTML = "";
+  gameScore.innerText = COUNT;
+
+  addItem("carrot", COUNT, "img/carrot.png");
+  addItem("bug", COUNT, "img/bug.png");
 }
 
 function addItem(className, count, imgPath) {
   const x1 = 0;
   const y1 = 0;
-  const x2 = fieldRect.width - imageSize;
-  const y2 = fieldRect.height - imageSize;
+  const x2 = fieldRect.width - IMAGE_SIZE;
+  const y2 = fieldRect.height - IMAGE_SIZE;
 
   for (let i = 0; i < count; i++) {
     const item = document.createElement("img");
@@ -31,5 +97,3 @@ function addItem(className, count, imgPath) {
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
-
-initGame();
